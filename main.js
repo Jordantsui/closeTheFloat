94227400e2d7cad89f45acbcb80c862e0df2ef99
Jordantsui <jiehaocui@foxmail.com>
2019-03-05 22:35:27 +0800

    点击别处使浮层消失，修改了两个bug

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
 //在每一个$(clickMe).on('click', function()内部添加监听$(document).one('click', function()，打开浮层就监听，关闭浮层之后就不监听了
+
 $(clickMe).on('click', function() {
+  if(popover.style.display==='none'){
+      //bug1:如果没有这个判断，点击按钮使浮层消失后，再点击按钮，浮层不会出现
     $(popover).show()
     console.log('show')
     setTimeout(function() {
+      x=1
       console.log('添加 one click')
       $(popover).on('click', function(e) {
         e.stopPropagation()
       })
+      //bug2:如果没有以上三句，点击浮层，浮层也会消失
       $(document).one('click', function() {
+        x=0
         console.log('我觉得这里不会执行')
         console.log('hide')
         $(popover).hide()
       })
     }, 0)
+  }else{
+    $(popover).hide()
+  }
   })
   
   //如果不加setTimeout，则$(document).one('click', function()会在之后立刻触发；加上setTimeout之后，会等到此冒泡过程完了之后再执行setTimeout里面的代码，而此时，事件过程已结束，因此$(document).one('click', function()不会执行
